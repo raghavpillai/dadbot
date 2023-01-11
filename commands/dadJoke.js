26108a32c02cb5fb2dcfa722921309cd583ab382
rishixyz9 <69014537+rishixyz9@users.noreply.github.com>
2023-01-10 20:50:44 -0600

    dadjoke enhancement

--- commands/dadJoke.js
+++ commands/dadJoke.js
@@ -1,12 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const jokes = require('../data/dadjokes.json')
+
+function exampleEmbed(title, joke){
+	return new EmbedBuilder()
+	.setColor(0x0099FF)
+	.setTitle(title)
+	.addFields(
+		{ name: 'Click to Reveal!', value: `||${joke}||`},
+	)
+	.setTimestamp()
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('dadjoke')
-		.setDescription('Replies with a dadjoke'),
+		.setDescription('Replies with a Dad Joke or a Bad Joke?'),
 	async execute(interaction) {
-        let index = Math.floor(Math.random() * jokes.length)
-		await interaction.reply(jokes[index]);
+		const url = 'https://www.reddit.com/r/dadjokes/random.json?t=all'
+		const joke = (await (await fetch(url)).json());
+		interaction.reply({embeds: [exampleEmbed(joke[0].data.children[0].data.title, joke[0].data.children[0].data.selftext)]})
 	},
 };
